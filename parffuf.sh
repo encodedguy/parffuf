934bb87278f79379a6bab8ef236953c9502701c8
encodedguy <65826142+encodedguy@users.noreply.github.com>
2023-01-10 03:06:46 -0800

    Sequential mode fuzzing
    Users can fuzz many hosts with a wordlist at customised rates.

---
+++ parffuf.sh
@@ -0,0 +1,18 @@
+#echo $# arguments 
+if [ $# -ne 3 ]; 
+    then echo "[Syntax:] bash dirasha.sh <host.txt> <wordlist.txt> <rate>"
+    exit
+fi
+
+echo [File Name:] $1
+echo [Wordlist:] $2
+echo [Fuzz Rate:] $3 rps
+echo "[Mode:] Sequential"
+
+echo
+
+while read host; do
+	name=$(echo $host | cut -d'/' -f3)
+	echo [Fuzzing] $host
+	ffuf -w $2 -u $host/FUZZ -o $name.json -rate $3 &> /dev/null
+done < $1
